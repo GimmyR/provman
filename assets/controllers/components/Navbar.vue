1563394254c52ab15da2187cdf5714e95b256c87
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-11-09 10:16:11 +0300

    Creating navbar component

---
+++ assets/controllers/components/Navbar.vue
@@ -0,0 +1,54 @@
+<script setup>
+defineProps({ username: String });
+</script>
+
+<template>
+    <nav class="navbar navbar-expand navbar-dark bg-purple navbar-custom">
+        <div class="container-fluid">
+            <a href="/" class="navbar-brand fw-bold">ProvMan</a>
+            <div class="collapse navbar-collapse d-flex flex-row justify-content-between">
+                <form class="d-flex mx-auto" role="search">
+                    <div class="input-group">
+                        <select class="form-select navbar-select" aria-label="Default select">
+                            <option value="1" selected>In Stock</option>
+                            <option value="2" selected>In Use</option>
+                            <option value="3" selected>Finished</option>
+                        </select>
+                        <input type="search" class="form-control" placeholder="Search provision" aria-label="Search"/>
+                        <button type="button" class="btn btn-light">
+                            <i class="bi bi-search"></i>
+                        </button>
+                    </div>
+                </form>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link bi bi-plus-circle fs-4"></a>
+                    </li>
+                    <li class="nav-item">
+                        <div class="dropdown">
+                            <a href="#" class="nav-link bi bi-person-circle fs-4" data-bs-toggle="dropdown" aria-expanded="false"></a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li><h6 class="dropdown-header">{{ username }}</h6></li>
+                                <li><hr class="dropdown-divider"/></li>
+                                <li><a href="#" class="dropdown-item">Settings</a></li>
+                                <li><a href="/logout" class="dropdown-item">Logout</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style>
+    .navbar-custom {
+        height: 54px;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    .navbar-select {
+        max-width: 30%;
+    }
+</style>
