f4e5fb11e2da16b0d228dfbd92bdead20913f0f3
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-11-20 08:51:50 +0300

    Removing image when removing provision

--- src/Controller/ApiEditProvisionsController.php
+++ src/Controller/ApiEditProvisionsController.php
@@ -8,6 +8,7 @@ use App\Repository\ProvisionRepository;
 use App\Repository\StateRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,28 +17,28 @@ use Symfony\Component\Routing\Annotation\Route;
 class ApiEditProvisionsController extends AbstractController
 {
     #[Route('/api/edit-provisions', name: 'api_edit_provisions')]
-    public function index(Request $request, EntityManagerInterface $entityManager, ProvisionRepository $provRep): JsonResponse
+    public function index(Request $request, EntityManagerInterface $entityManager, ProvisionRepository $provRep, Filesystem $filesystem): JsonResponse
     {
-        $provisions = $this->editProvisions($request, $entityManager, $provRep);
+        $provisions = $this->editProvisions($request, $entityManager, $provRep, $filesystem);
         
         return $this->json([
             "provisions" => $provisions
         ]);
     }
 
-    private function editProvisions(Request $request, EntityManagerInterface $entityManager, ProvisionRepository $provRep): array {
+    private function editProvisions(Request $request, EntityManagerInterface $entityManager, ProvisionRepository $provRep, Filesystem $filesystem): array {
 
         $provisions = json_decode($request->getContent(), true);
         $states = $entityManager->getRepository(State::class)->findAll();
 
-        $provisions = $this->removeProvisions($states, $provisions, $entityManager, $provRep);
+        $provisions = $this->removeProvisions($states, $provisions, $entityManager, $provRep, $filesystem);
         $this->saveProvisions($states, $provisions, $entityManager, $provRep);
 
         return $this->reorderProvisions($states, $provRep);
 
     }
 
-    private function removeProvisions(array $states, array $provisions, EntityManagerInterface $entityManager, ProvisionRepository $provRep): array {
+    private function removeProvisions(array $states, array $provisions, EntityManagerInterface $entityManager, ProvisionRepository $provRep, Filesystem $filesystem): array {
 
         for($i = 0; $i < count($states); $i++) {
 
@@ -45,6 +46,7 @@ class ApiEditProvisionsController extends AbstractController
 
                 if(isset($provisions[$i][$j]["remove"]) && $provisions[$i][$j]["remove"]) {
                     $provision = $provRep->find($provisions[$i][$j]["id"]);
+                    $filesystem->remove('./provisions/' .$provision->getImage());
                     $entityManager->remove($provision);
                     $entityManager->flush();
                     array_splice($provisions[$i], $j, 1);
