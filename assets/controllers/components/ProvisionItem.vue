ecd9956874fc35aeea906a24d3b2309ebeb5e5f1
Gimmy Razafimbelo <gimmyarazafimbelo2@gmail.com>
2023-11-20 08:51:23 +0300

    Displaying correctly image

--- assets/controllers/components/ProvisionItem.vue
+++ assets/controllers/components/ProvisionItem.vue
@@ -7,17 +7,17 @@ const classes = reactive({ display: "d-none" });
 </script>
 
 <template>
-    <div class="col-12 shadow mb-4 pb-3 d-flex flex-column" @mouseenter="() => classes.display = 'd-inline'" @mouseleave="() => classes.display = 'd-none'">
+    <div class="col-12 shadow-lg mb-4 d-flex flex-column" @mouseenter="() => classes.display = 'd-inline'" @mouseleave="() => classes.display = 'd-none'">
         <div class="d-flex flex-row justify-content-end close-button-div">
             <button type="button" @click="() => edit(provision)" class="btn btn-danger rounded-0" v-bind:class="classes.display" data-bs-toggle="modal" data-bs-target="#edit-provision-modal">
                 <i class="bi bi-pencil-fill"></i>
             </button>
         </div>
-        <div>
+        <div class="pb-4">
             <h2 class="fs-5 text-center">{{ provision.name }}</h2>
         </div>
         <div class="d-flex flex-row justify-content-center">
-            <img v-if="provision.image != null" v-bind:src="'/provisions/' + provision.image"/>
+            <img v-if="provision.image != null" v-bind:src="'/provisions/' + provision.image" class="img-fluid"/>
         </div>
     </div>
 </template>
